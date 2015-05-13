def0f025bda1d57c700e36043c86830a029c908d
Rana Mansoor Akbar Khan <mansoor@sutlej.net>
2015-05-13 19:42:42 +0500

    more cleanup

--- pageparts/header.php
+++ pageparts/header.php
@@ -1,13 +1,12 @@
 ﻿<head>
 <title>LWR Clubs and Organizations</title>
 <link rel="STYLESHEET" type="text/css" href="css/lwr.css">
-<link href="dist/css/select2.min.css" rel="stylesheet" />
-<link href="dist/css/select2.css" rel="stylesheet" />
+<link href="css/select2.min.css" rel="stylesheet" />
 
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
 <?php include_once('js-google.php'); ?>
 <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-<script src="dist/js/select2.min.js"></script>
+<script src="js/select2.min.js"></script>
 
 <script type="text/javascript" src="js/lwr.js"></script>	
 <script>
